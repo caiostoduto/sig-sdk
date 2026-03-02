68d98e7bbf23b2cca4c347621fde0ac1f06a0361
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2026-03-01 22:45:31 -0300

    ci: add GitHub Actions workflow for publishing and update package name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,5 @@
  * Main entry point for the SIGAA SDK
  * @packageDocumentation
  */
-export const version = "1.0.1";
-
 export * from "./sigaa/ufabc/client";
 export * from "./sigaa/ufabc/session";
